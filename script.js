5fbb3be6471ced417bc5b8a50cfc1a48f1e11e1b
vince <paulag10987@gmail.com>
2026-02-14 12:37:55 +0800

    chore(js): add dom elements to the js

---
+++ script.js
@@ -0,0 +1,6 @@
+const introText = document.getElementById('intro-text');
+const messageText = document.getElementById('message-text');
+const garden = document.getElementById('garden');
+const background = document.getElementById('background');
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
